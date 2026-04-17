49e12634ff65a2ae17b23a0f51f1dc03e042321a
Vinod <vinodsv1618@gmail.com>
2026-04-17 21:19:47 +0530

    hello

--- main.tf
+++ main.tf
@@ -28,3 +28,78 @@ resource "aws_subnet" "public" {
         var.public_subnet_tags
         )
 }
+
+
+resource "aws_subnet" "Private" {
+  count = length(var.private_subnet_cidr)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+        local.common_tags,
+        {
+          Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
+        },
+        var.private_subnet_tags
+        )
+}
+
+
+
+resource "aws_subnet" "databse" {
+  count = length(var.database_subnet_cidr)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.database_subnet_cidr[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+        local.common_tags,
+        {
+          Name = "${var.project}-${var.environment}-databse-${local.az_names[count.index]}"
+        },
+        var.database_subnet_tags
+        )
+}
+
+
+# route tables
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+        local.common_tags,
+        {
+          Name = "${var.project}-${var.environment}-public"
+        },
+        var.public_route_table_tags
+        )
+}
+
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+        local.common_tags,
+        {
+          Name = "${var.project}-${var.environment}-private"
+        },
+        var.private_route_table_tags
+        )
+}
+
+
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+        local.common_tags,
+        {
+          Name = "${var.project}-${var.environment}-database"
+        },
+        var.database_route_table_tags
+        )
+}
--- variables.tf
+++ variables.tf
@@ -32,3 +32,41 @@ variable "public_subnet_tags"{
     type = map
     default = {}
 }
+
+
+variable "private_subnet_cidr"{
+    type = list
+    default = ["10.0.11.0/24","10.0.12.0/24"]
+}
+
+variable "private_subnet_tags"{
+    type = map
+    default = {}
+}
+
+
+variable "database_subnet_cidr"{
+    type = list
+    default = ["10.0.21.0/24","10.0.22.0/24"]
+}
+
+variable "database_subnet_tags"{
+    type = map
+    default = {}
+}
+
+variable "public_route_table_tags"{
+    type = map
+    default ={}
+}
+
+variable "private_route_table_tags"{
+    type = map
+    default ={}
+}
+
+
+variable "database_route_table_tags"{
+    type = map
+    default ={}
+}
