66ce4017c4073eae3a70da475a77a85c264c4e35
Vinod <vinodsv1618@gmail.com>
2026-04-17 22:36:17 +0530

    hello

--- variables.tf
+++ variables.tf
@@ -70,3 +70,14 @@ variable "database_route_table_tags"{
     type = map
     default ={}
 }
+
+
+variable "eip_tags"{
+    type = map
+    default = {}
+}
+
+variable "nat_gateway_tags"{
+    type = map
+    default = {}
+}
