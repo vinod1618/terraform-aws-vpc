450a0763c27b42367b08ccb3f0585ed573e8352a
Vinod <vinodsv1618@gmail.com>
2026-04-17 16:14:51 +0530

    hello

--- main.tf
+++ main.tf
@@ -9,7 +9,5 @@ resource "aws_vpc" "main" {
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id  # VPC association
 
-  tags = {
-    Name = local.final_igw_tags
-  }
+  tags = local.final_igw_tags
 }
