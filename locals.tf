d5d543db1cc39479427c8096e532b42348d32e87
Vinod <vinodsv1618@gmail.com>
2026-04-17 17:11:23 +0530

    hello

--- locals.tf
+++ locals.tf
@@ -20,4 +20,6 @@ locals{
     },
     var.igw_tags
 )
+
+    az_names = slice(data.aws_availability_zones.available.names,0,2)
 }
--- main.tf
+++ main.tf
@@ -13,13 +13,18 @@ resource "aws_internet_gateway" "main" {
 }
 
 
-# resource "aws_subnet" "public" {
-#   count = length(var.public_subnet_cidr)
-#   vpc_id            = aws_vpc.main.id
-#   cidr_block        = var.public_subnet_cidr[count.index]
-#   availability_zone = "us-east-1a"
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidr)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidr[count.index]
+  availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true
 
-#   tags = {
-#     Name = "MainSubnet"
-#   }
-# }
+  tags = merge(
+        local.common_tags,
+        {
+          Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+        },
+        var.public_subnet_tags
+        )
+}
