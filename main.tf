d8fbbd2b66a1bf803c827f2c2d7158058da21aad
Vinod <vinodsv1618@gmail.com>
2026-04-17 22:37:09 +0530

    hello

--- main.tf
+++ main.tf
@@ -154,7 +154,7 @@ resource "aws_route" "private" {
 
 
 
-resource "aws_route" "public" {
+resource "aws_route" "database" {
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.main.id
