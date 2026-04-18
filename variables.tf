a60e0a23ab60be403ee39feb1bc8778b1c58aa74
Vinod <vinodsv1618@gmail.com>
2026-04-18 12:27:24 +0530

    hello

--- variables.tf
+++ variables.tf
@@ -28,7 +28,7 @@ variable "public_subnet_cidr"{
     default = ["10.0.1.0/24","10.0.2.0/24"]
 }
 
-variable "private_subnet_tags"{
+variable "public_subnet_tags"{
     type = map
     default = {}
 }
