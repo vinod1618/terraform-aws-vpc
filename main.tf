33ede5191526bb0a9732f38abf05d35991667ecd
Vinod <vinodsv1618@gmail.com>
2026-04-18 12:21:09 +0530

    hello

--- main.tf
+++ main.tf
@@ -160,3 +160,25 @@ resource "aws_route" "database" {
   gateway_id = aws_internet_gateway.main.id
 }
 
+# associating with the route table
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidr)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidr)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidr)
+  subnet_id      = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database.id
+}
+
