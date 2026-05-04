be34dd1f5dbcbf4d1090fea8f21fef56bf92cf46
Vinod <vinodsv1618@gmail.com>
2026-05-04 17:11:44 +0530

    hello

--- peering.tf
+++ peering.tf
@@ -39,6 +39,6 @@ resource "aws_route" "default_peering" {
   count = var.is_peering_required ? 1 : 0
   route_table_id            = data.aws_route_table.default.id
   destination_cidr_block    = var.vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection_default[count.index].id
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
