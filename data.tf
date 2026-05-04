ced52dfb05a3d45bf1d0e2e9c96fe90d510974db
Vinod <vinodsv1618@gmail.com>
2026-05-04 17:09:21 +0530

    hello

--- data.tf
+++ data.tf
@@ -6,3 +6,7 @@ data "aws_availability_zones" "available" {
 data "aws_vpc" "default" {
  default = true
 }
+
+data "aws_route_table" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
--- peering.tf
+++ peering.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_peering_connection" "default" {
   count = var.is_peering_required ? 1 : 0
-  # peer_owner_id = var.peer_owner_id
+   # peer_owner_id = var.peer_owner_id
   # target VPC :
   peer_vpc_id   = data.aws_vpc.default.id
 
@@ -24,3 +24,21 @@ resource "aws_vpc_peering_connection" "default" {
     
     )
 }
+
+
+resource "aws_route" "public_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+
+
+resource "aws_route" "default_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = data.aws_route_table.default.id
+  destination_cidr_block    = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection_default[count.index].id
+}
+
