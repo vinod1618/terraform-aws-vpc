e1067750ff2bf202309b5d7523d1fe70024b5484
Vinod <vinodsv1618@gmail.com>
2026-05-08 20:16:56 +0530

    hello

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "az_info"{
     value = data.aws_availability_zones.available
 }
+
+output "vpc_id"{
+    value = aws_vpc.main.id
+}
+
+output "public_subnet_id"{
+    value = aws_subnet.public[*].id
+}
+
+output "private_subnet_id"{
+    value = aws_subnet.private[*].id
+}
+
+output "database_subnet_id"{
+    value = aws_subnet.database[*].id
+}
--- peering.tf
+++ peering.tf
@@ -33,6 +33,21 @@ resource "aws_route" "public_peering" {
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
+resource "aws_route" "private_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+
+resource "aws_route" "database_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
 
 
 resource "aws_route" "default_peering" {
