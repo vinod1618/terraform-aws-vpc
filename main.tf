3548709e28503b4d75635cb1871273aa636b857f
Vinod <vinodsv1618@gmail.com>
2026-07-08 22:53:47 +0530

    hello

--- main.tf
+++ main.tf
@@ -181,3 +181,17 @@ resource "aws_route_table_association" "database" {
   subnet_id      = aws_subnet.database[count.index].id
   route_table_id = aws_route_table.database.id
 }
+
+
+resource "aws_db_subnet_group" "roboshop" {
+  name       = "${var.project}-${var.environment}"
+  subnet_ids = [aws_subnet.database[*].id]
+
+  tags = merge(
+        local.common_tags,
+        {
+          Name = "${var.project}-${var.environment}"
+        },
+        var.nat_gateway_tags
+        )
+}
