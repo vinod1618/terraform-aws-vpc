5923adaf0427ba0574f68e909a4c8c11be8608e6
Vinod <vinodsv1618@gmail.com>
2026-07-08 22:58:15 +0530

    hello

--- main.tf
+++ main.tf
@@ -185,7 +185,7 @@ resource "aws_route_table_association" "database" {
 
 resource "aws_db_subnet_group" "roboshop" {
   name       = "${var.project}-${var.environment}"
-  subnet_ids = [aws_subnet.database[*].id]
+  subnet_ids = [aws_subnet.database[0].id,aws_subnet.database[1].id]
 
   tags = merge(
         local.common_tags,
